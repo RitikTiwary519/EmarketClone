c1f41dbe368aa4f6021d2930b3b9c185887f04c9
RarefiedAsh <ritiktiwaryyo@gmail.com>
2024-10-27 01:57:11 +0530

    carouser functionality added and server set

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+
 import Header from './component/header/Header';
+import Home from './component/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div style={{marginTop:54}}>
+        <Home />
+      </div>
     </div>
   );
 }
